1c85170ce9d8bf125fd96747ebd8fdd45afa8268
Theodor Thornhill <theodor.thornhill@tv2.no>
2024-11-07 15:08:40 +0100

    Build librdkafka with zig

--- build.zig
+++ build.zig
@@ -2,156 +2,66 @@ const std = @import("std");
 const this = @This();
 
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{});
 
-    const kafka = b.addSharedLibrary(.{
-        .name = "zig-kafka",
-        .root_source_file = b.path("lib/kafka.zig"),
-        .optimize = optimize,
+    const upstream = b.dependency("upstream", .{
         .target = target,
+        .optimize = optimize,
     });
-
-    kafka.linkLibC();
-    // kafka.linkSystemLibrary("sasl2");
-    // kafka.linkSystemLibrary("curl");
-    kafka.linkSystemLibrary("rdkafka");
 
     const zlib_dep = b.dependency("zlib", .{
         .target = target,
         .optimize = optimize,
     });
-    kafka.linkLibrary(zlib_dep.artifact("z"));
 
-    const libressl_dependency = b.dependency("libressl", .{
+    const zstd_dep = b.dependency("zstd", .{
         .target = target,
         .optimize = optimize,
-        .@"enable-asm" = true,
     });
-    kafka.linkLibrary(libressl_dependency.artifact("ssl"));
 
-    // const cflags = &[_][]const u8{
-    //     "-g",
-    //     "-O3",
-    //     "-fPIC",
-    //     // Too much ub in librdkafka to be funny
-    //     "-fno-sanitize=all",
-    //     "-Wall",
-    //     "-Wsign-compare",
-    //     "-Wfloat-equal",
-    //     "-Wpointer-arith",
-    //     "-Wcast-align",
-    // };
-    // kafka.addIncludePath(b.path("c/librdkafka/"));
-    // kafka.addIncludePath(b.path("c/librdkafka/src"));
-    // kafka.addIncludePath(b.path("c/librdkafka/src/nanopb"));
-    // kafka.addIncludePath(b.path("c/librdkafka/src/opentelemetry"));
-    // kafka.addCSourceFiles(.{
-    //     .files = &.{
-    //         "c/librdkafka/src/nanopb/pb_common.c",
-    //         "c/librdkafka/src/nanopb/pb_decode.c",
-    //         "c/librdkafka/src/nanopb/pb_encode.c",
-    //         "c/librdkafka/src/opentelemetry/common.pb.c",
-    //         "c/librdkafka/src/opentelemetry/metrics.pb.c",
-    //         "c/librdkafka/src/opentelemetry/resource.pb.c",
-    //         "c/librdkafka/src/lz4frame.c",
-    //         "c/librdkafka/src/lz4.c",
-    //         "c/librdkafka/src/lz4hc.c",
-    //         "c/librdkafka/src/snappy.c",
-    //         "c/librdkafka/src/cJSON.c",
-    //         "c/librdkafka/src/rdmurmur2.c",
-    //         "c/librdkafka/src/crc32c.c",
-    //         "c/librdkafka/src/rdstring.c",
-    //         "c/librdkafka/src/rdregex.c",
-    //         "c/librdkafka/src/rdrand.c",
-    //         "c/librdkafka/src/rdxxhash.c",
-    //         "c/librdkafka/src/rdavl.c",
-    //         "c/librdkafka/src/rdvarint.c",
-    //         "c/librdkafka/src/rddl.c",
-    //         "c/librdkafka/src/rdbase64.c",
-    //         "c/librdkafka/src/rdaddr.c",
-    //         "c/librdkafka/src/rdfnv1a.c",
-    //         "c/librdkafka/src/rdhttp.c",
-    //         "c/librdkafka/src/rdunittest.c",
-    //         "c/librdkafka/src/rdgz.c",
-    //         "c/librdkafka/src/rdcrc32.c",
-    //         "c/librdkafka/src/rdbuf.c",
-    //         "c/librdkafka/src/rdlog.c",
-    //         "c/librdkafka/src/rdports.c",
-    //         "c/librdkafka/src/rdmap.c",
-    //         "c/librdkafka/src/rdlist.c",
-    //         "c/librdkafka/src/rdhdrhistogram.c",
-    //         "c/librdkafka/src/rdkafka.c",
-    //         "c/librdkafka/src/rdkafka_admin.c",
-    //         "c/librdkafka/src/rdkafka_assignment.c",
-    //         "c/librdkafka/src/rdkafka_assignor.c",
-    //         "c/librdkafka/src/rdkafka_aux.c",
-    //         "c/librdkafka/src/rdkafka_background.c",
-    //         "c/librdkafka/src/rdkafka_broker.c",
-    //         "c/librdkafka/src/rdkafka_buf.c",
-    //         "c/librdkafka/src/rdkafka_cert.c",
-    //         "c/librdkafka/src/rdkafka_cgrp.c",
-    //         "c/librdkafka/src/rdkafka_conf.c",
-    //         "c/librdkafka/src/rdkafka_coord.c",
-    //         "c/librdkafka/src/rdkafka_error.c",
-    //         "c/librdkafka/src/rdkafka_event.c",
-    //         "c/librdkafka/src/rdkafka_feature.c",
-    //         "c/librdkafka/src/rdkafka_fetcher.c",
-    //         "c/librdkafka/src/rdkafka_header.c",
-    //         "c/librdkafka/src/rdkafka_idempotence.c",
-    //         "c/librdkafka/src/rdkafka_interceptor.c",
-    //         "c/librdkafka/src/rdkafka_lz4.c",
-    //         "c/librdkafka/src/rdkafka_metadata.c",
-    //         "c/librdkafka/src/rdkafka_metadata_cache.c",
-    //         "c/librdkafka/src/rdkafka_mock.c",
-    //         "c/librdkafka/src/rdkafka_mock_cgrp.c",
-    //         "c/librdkafka/src/rdkafka_mock_handlers.c",
-    //         "c/librdkafka/src/rdkafka_msg.c",
-    //         "c/librdkafka/src/rdkafka_msgset_reader.c",
-    //         "c/librdkafka/src/rdkafka_msgset_writer.c",
-    //         "c/librdkafka/src/rdkafka_offset.c",
-    //         "c/librdkafka/src/rdkafka_op.c",
-    //         "c/librdkafka/src/rdkafka_partition.c",
-    //         "c/librdkafka/src/rdkafka_pattern.c",
-    //         "c/librdkafka/src/rdkafka_plugin.c",
-    //         "c/librdkafka/src/rdkafka_queue.c",
-    //         "c/librdkafka/src/rdkafka_range_assignor.c",
-    //         "c/librdkafka/src/rdkafka_request.c",
-    //         "c/librdkafka/src/rdkafka_roundrobin_assignor.c",
-    //         "c/librdkafka/src/rdkafka_sasl.c",
-    //         "c/librdkafka/src/rdkafka_sasl_cyrus.c",
-    //         "c/librdkafka/src/rdkafka_sasl_oauthbearer.c",
-    //         "c/librdkafka/src/rdkafka_sasl_oauthbearer_oidc.c",
-    //         "c/librdkafka/src/rdkafka_sasl_plain.c",
-    //         "c/librdkafka/src/rdkafka_sasl_scram.c",
-    //         //"c/librdkafka/src/rdkafka_sasl_win32.c",
-    //         "c/librdkafka/src/rdkafka_ssl.c",
-    //         "c/librdkafka/src/rdkafka_sticky_assignor.c",
-    //         "c/librdkafka/src/rdkafka_subscription.c",
-    //         "c/librdkafka/src/rdkafka_telemetry.c",
-    //         "c/librdkafka/src/rdkafka_telemetry_decode.c",
-    //         "c/librdkafka/src/rdkafka_telemetry_encode.c",
-    //         "c/librdkafka/src/rdkafka_timer.c",
-    //         "c/librdkafka/src/rdkafka_topic.c",
-    //         "c/librdkafka/src/rdkafka_transport.c",
-    //         "c/librdkafka/src/rdkafka_txnmgr.c",
-    //         // "c/librdkafka/src/rdkafka_zstd.c",
-    //         "c/librdkafka/src/tinycthread.c",
-    //         "c/librdkafka/src/tinycthread_extra.c",
-    //     },
-    //     .flags = cflags,
-    // });
+    const rdkafka = b.addStaticLibrary(.{
+        .name = "rdkafka",
+        .optimize = optimize,
+        .target = target,
+    });
 
-    b.installArtifact(kafka);
+    const configure = b.option(bool, "configure", "Whether to run ./configure or not") orelse true;
 
+    if (configure) {
+        const configure_cmd = upstream.builder.addSystemCommand(&[_][]const u8{
+            "./configure",
+            "--install-deps",
+            "--source-deps-only",
+            "--enable-static",
+        });
+        configure_cmd.step.dependOn(upstream.builder.default_step);
+        rdkafka.step.dependOn(&configure_cmd.step);
+    }
+
+    rdkafka.addCSourceFiles(.{
+        .root = upstream.path("."),
+        .files = sources,
+        .flags = cflags,
+    });
+
+    rdkafka.addCSourceFiles(.{
+        .root = upstream.path("src"),
+        .files = other_sources,
+        .flags = cflags,
+    });
+
+    rdkafka.addIncludePath(upstream.path("src"));
+
+    rdkafka.linkLibC();
+    rdkafka.linkLibrary(zlib_dep.artifact("z"));
+    rdkafka.linkLibrary(zstd_dep.artifact("zstd"));
+    rdkafka.addObjectFile(upstream.path("mklove/deps/dest/usr/lib/libcurl.a"));
+    rdkafka.addObjectFile(upstream.path("mklove/deps/dest/usr/lib64/libssl.a"));
+    rdkafka.addObjectFile(upstream.path("mklove/deps/dest/usr/lib64/libcrypto.a"));
+
+    b.installArtifact(rdkafka);
 
     const zk = b.addModule("zig-kafka", .{
         .root_source_file = b.path("lib/kafka.zig"),
@@ -159,13 +69,11 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    zk.linkLibrary(kafka);
-    zk.addIncludePath(b.path("c/librdkafka/"));
-    zk.addIncludePath(b.path("c/librdkafka/src"));
-    zk.addIncludePath(b.path("c/librdkafka/src/nanopb"));
-    zk.addIncludePath(b.path("c/librdkafka/src/opentelemetry"));
-    // zk.addImport("zig-kafka", &kafka.root_module);
-
+    zk.linkLibrary(rdkafka);
+    zk.addIncludePath(upstream.path("src"));
+    zk.addObjectFile(upstream.path("mklove/deps/dest/usr/lib/libcurl.a"));
+    zk.addObjectFile(upstream.path("mklove/deps/dest/usr/lib64/libssl.a"));
+    zk.addObjectFile(upstream.path("mklove/deps/dest/usr/lib64/libcrypto.a"));
 
     const exe_unit_tests = b.addTest(.{
         .root_source_file = b.path("lib/kafka.zig"),
@@ -173,14 +81,119 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    exe_unit_tests.addIncludePath(b.path("c/librdkafka/"));
-    exe_unit_tests.addIncludePath(b.path("c/librdkafka/src"));
-    exe_unit_tests.addIncludePath(b.path("c/librdkafka/src/nanopb"));
-    exe_unit_tests.addIncludePath(b.path("c/librdkafka/src/opentelemetry"));
-    // exe_unit_tests.root_module.addImport("kafka", &kafka.root_module);
+    exe_unit_tests.linkLibrary(rdkafka);
+    zk.addIncludePath(upstream.path("src"));
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 }
+
+const cflags = &[_][]const u8{
+    "-g",
+    "-O3",
+    "-fno-sanitize=undefined",
+    "-fPIC",
+    "-Wall",
+    "-Wsign-compare",
+    "-Wfloat-equal",
+    "-Wpointer-arith",
+    "-Wcast-align",
+};
+
+const sources: []const []const u8 = &.{
+    "src/lz4frame.c",
+    "src/lz4.c",
+    "src/lz4hc.c",
+    "src/snappy.c",
+    "src/cJSON.c",
+    "src/rdmurmur2.c",
+    "src/crc32c.c",
+    "src/rdstring.c",
+    "src/rdregex.c",
+    "src/rdrand.c",
+    "src/rdxxhash.c",
+    "src/rdavl.c",
+    "src/rdvarint.c",
+    "src/rddl.c",
+    "src/rdbase64.c",
+    "src/rdaddr.c",
+    "src/rdfnv1a.c",
+    "src/rdhttp.c",
+    "src/rdunittest.c",
+    "src/rdgz.c",
+    "src/rdcrc32.c",
+    "src/rdbuf.c",
+    "src/rdlog.c",
+    "src/rdports.c",
+    "src/rdmap.c",
+    "src/rdlist.c",
+    "src/rdhdrhistogram.c",
+    "src/rdkafka.c",
+    "src/rdkafka_admin.c",
+    "src/rdkafka_assignment.c",
+    "src/rdkafka_assignor.c",
+    "src/rdkafka_aux.c",
+    "src/rdkafka_background.c",
+    "src/rdkafka_broker.c",
+    "src/rdkafka_buf.c",
+    "src/rdkafka_cert.c",
+    "src/rdkafka_cgrp.c",
+    "src/rdkafka_conf.c",
+    "src/rdkafka_coord.c",
+    "src/rdkafka_error.c",
+    "src/rdkafka_event.c",
+    "src/rdkafka_feature.c",
+    "src/rdkafka_fetcher.c",
+    "src/rdkafka_header.c",
+    "src/rdkafka_idempotence.c",
+    "src/rdkafka_interceptor.c",
+    "src/rdkafka_lz4.c",
+    "src/rdkafka_metadata.c",
+    "src/rdkafka_metadata_cache.c",
+    "src/rdkafka_msg.c",
+    "src/rdkafka_msgset_reader.c",
+    "src/rdkafka_msgset_writer.c",
+    "src/rdkafka_offset.c",
+    "src/rdkafka_op.c",
+    "src/rdkafka_partition.c",
+    "src/rdkafka_pattern.c",
+    "src/rdkafka_plugin.c",
+    "src/rdkafka_queue.c",
+    "src/rdkafka_range_assignor.c",
+    "src/rdkafka_request.c",
+    "src/rdkafka_roundrobin_assignor.c",
+    "src/rdkafka_sasl.c",
+    "src/rdkafka_sasl_cyrus.c",
+    "src/rdkafka_sasl_oauthbearer.c",
+    "src/rdkafka_sasl_oauthbearer_oidc.c",
+    "src/rdkafka_sasl_plain.c",
+    "src/rdkafka_sasl_scram.c",
+    "src/rdkafka_ssl.c",
+    "src/rdkafka_sticky_assignor.c",
+    "src/rdkafka_subscription.c",
+    "src/rdkafka_timer.c",
+    "src/rdkafka_topic.c",
+    "src/rdkafka_transport.c",
+    "src/rdkafka_txnmgr.c",
+    "src/rdkafka_zstd.c",
+    "src/tinycthread.c",
+    "src/tinycthread_extra.c",
+    "src/rdkafka_telemetry.c",
+    "src/rdkafka_telemetry_decode.c",
+    "src/rdkafka_telemetry_encode.c",
+    // "rdkafka_sasl_win32.c",
+    "src/rdkafka_mock.c",
+    "src/rdkafka_mock_cgrp.c",
+    "src/rdkafka_mock_handlers.c",
+};
+
+const other_sources: []const []const u8 = &.{
+    "nanopb/pb_common.c",
+    "nanopb/pb_decode.c",
+    "nanopb/pb_encode.c",
+    "opentelemetry/common.pb.c",
+    "opentelemetry/metrics.pb.c",
+    "opentelemetry/resource.pb.c",
+};
