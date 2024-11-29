d330c3856bb44761129529ef382633d466c852fd
Theodor Thornhill <theodor.thornhill@tv2.no>
2024-11-29 15:48:12 +0100

    Link libc to tests

--- build.zig
+++ build.zig
@@ -26,11 +26,11 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe_unit_tests.linkLibC();
     exe_unit_tests.addIncludePath(upstream.path("src"));
     exe_unit_tests.root_module.addImport("zig-kafka", zk);
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
